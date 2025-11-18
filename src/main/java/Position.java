072e517063811f77a20e762f3068f5e46c65eb7e
Mathis Sauvage <165672108+SmasBalloon@users.noreply.github.com>
2025-11-18 18:21:27 +0100

    Ajout de la classe Position

---
+++ src/main/java/Position.java
@@ -0,0 +1,38 @@
+public class Position {
+    private Integer x;
+    private Integer y;
+
+    public Position(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Position position = (Position) obj;
+        return x.equals(position.x) && y.equals(position.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+}
