49f21b93412fa2f02d66213243e34fbbce1df908
Mathis Sauvage <165672108+SmasBalloon@users.noreply.github.com>
2025-11-18 18:20:31 +0100

    Ajout de la classe BackwardMovement

---
+++ src/main/java/BackwardMovement.java
@@ -0,0 +1,24 @@
+public class BackwardMovement implements MovementStrategy {
+    @Override
+    public Position calculateNewPosition(Position currentPosition, Orientation orientation) {
+        int newX = currentPosition.getX();
+        int newY = currentPosition.getY();
+
+        switch (orientation) {
+            case NORTH:
+                newY--;
+                break;
+            case SOUTH:
+                newY++;
+                break;
+            case EAST:
+                newX--;
+                break;
+            case WEST:
+                newX++;
+                break;
+        }
+
+        return new Position(newX, newY);
+    }
+}
